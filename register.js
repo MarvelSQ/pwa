c2ee54ba3a7b9854606f322519c9d2eef804a7ec
Sun <webqiang.sun@hypers.com>
2018-01-10 18:24:10 +0800

    update sw.js

--- register.js
+++ register.js
@@ -1,9 +1,8 @@
 console.log('register sw');
 
 if (navigator.serviceWorker != null) {
-  navigator.serviceWorker.register('sw.8.js')
+  navigator.serviceWorker.register('sw.js')
   .then(function(registration) {
-    console.log(registration);
     console.log('Registered events at scope: ',new Date(), registration.scope);
   });
 }
--- sw.js
+++ sw.js
@@ -1,22 +1,37 @@
-console.log('sw.8.js loaded');
-
-var cacheStorageKey = 'minimal-pwa-8'
+const version = 9;
+const cacheStorageKey = 'minimal-pwa-'+version;
 
 var cacheList = [
-  '/',
   "index.html",
   "index.js",
   "logo@2x.png",
   'ad.json',
-  'sw.8.js'
 ]
 
 self.addEventListener('install', e => {
-  console.log('sw install', e);
   e.waitUntil(
     caches.open(cacheStorageKey)
     .then(cache => cache.addAll(cacheList))
     .then(() => self.skipWaiting())
+  )
+})
+
+self.addEventListener('activate', function(e) {
+  console.log('sw activate');
+  e.waitUntil(
+    Promise.all(
+      [caches.keys().then(cacheNames => {
+        return cacheNames.map(name => {
+          if (name !== cacheStorageKey) {
+            return caches.delete(name)
+          }
+          return 0;
+        })
+      })]
+    ).then((res) => {
+      console.log(res)
+      return 0;
+    })
   )
 })
 
@@ -31,26 +46,3 @@ self.addEventListener('fetch', function(e) {
     })
   )
 })
-
-self.addEventListener('activate', function(e) {
-  console.log('sw activate');
-  e.waitUntil(
-    Promise.all(
-      [caches.keys().then(cacheNames => {
-        console.log('running')
-        return cacheNames.map(name => {
-          console.log('inner running')
-          if (name !== cacheStorageKey) {
-            return caches.delete(name)
-          }
-          return 0;
-        })
-      })]
-    ).then((res) => {
-      console.log(res)
-      return 0;
-    })
-  )
-})
-
-console.log(self)
